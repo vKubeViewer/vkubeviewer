2e684129a6bb4324d99640a5b1a434d270318a46
RichardPHarris <79415636+RichardPHarris@users.noreply.github.com>
2021-06-24 13:53:38 +0100

    Add files via upload

---
+++ controllers/fcdinfo_controller.go
@@ -0,0 +1,161 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/find"
+	"github.com/vmware/govmomi/property"
+	"github.com/vmware/govmomi/vim25"
+	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/types"
+	"github.com/vmware/govmomi/vslm"
+	k8serr "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	topologyv1 "vkubeviewer/api/v1"
+)
+
+// FCDInfoReconciler reconciles a FCDInfo object
+type FCDInfoReconciler struct {
+	client.Client
+	VC1    *vim25.Client
+	VC2    *govmomi.Client
+	Finder *find.Finder
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=topology.vkubeviewer.com,resources=fcdinfoes,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=topology.vkubeviewer.com,resources=fcdinfoes/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=topology.vkubeviewer.com,resources=fcdinfoes/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the FCDInfo object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.7.2/pkg/reconcile
+func (r *FCDInfoReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	ctx = context.Background()
+	log := r.Log.WithValues("FCDInfo", req.NamespacedName)
+
+	fcd := &topologyv1.FCDInfo{}
+	if err := r.Client.Get(ctx, req.NamespacedName, fcd); err != nil {
+		if !k8serr.IsNotFound(err) {
+			log.Error(err, "unable to fetch FCDInfo")
+		}
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	msg := fmt.Sprintf("received reconcile request for %q (namespace: %q)", fcd.GetName(), fcd.GetNamespace())
+	log.Info(msg)
+
+	//
+	// Find the datastores available on this vSphere Infrastructure
+	//
+
+	dss, err := r.Finder.DatastoreList(ctx, "*")
+	if err != nil {
+		log.Error(err, "FCDInfo: Could not get datastore list")
+		return ctrl.Result{}, err
+	} else {
+		msg := fmt.Sprintf("FCDInfo: Number of datastores found - %v", len(dss))
+		log.Info(msg)
+
+		pc := property.DefaultCollector(r.VC2.Client)
+		//
+		// "finder" only lists - to get really detailed info,
+		// Convert datastores into list of references
+		//
+		var refs []types.ManagedObjectReference
+		for _, ds := range dss {
+			refs = append(refs, ds.Reference())
+		}
+
+		//
+		// Retrieve name property for all datastore
+		//
+
+		var dst []mo.Datastore
+		err = pc.Retrieve(ctx, refs, []string{"name"}, &dst)
+		if err != nil {
+			log.Error(err, "FCDInfo: Could not get datastore info")
+			return ctrl.Result{}, err
+		}
+
+		m := vslm.NewObjectManager(r.VC1)
+
+		//
+		// -- Display the FCDs on each datastore (held in array dst)
+		//
+
+		var objids []types.ID
+		var idinfo *types.VStorageObject
+
+		for _, newds := range dst {
+			objids, err = m.List(ctx, newds)
+			//
+			// -- With the list of FCD Ids, we can get further information about the FCD retrievec in VStorageObject
+			//
+			for _, id := range objids {
+				idinfo, err = m.Retrieve(ctx, newds, id.Id)
+				//
+				// -- Note the TKGS Guest Clusters have a different PV ID
+				// -- to the one that is created for them in the Supervisor
+				// -- This only works for the Supervisor PV ID
+				//
+				if idinfo.Config.BaseConfigInfo.Name == fcd.Spec.PVId {
+					msg := fmt.Sprintf("FCDInfo: %v matches %v", idinfo.Config.BaseConfigInfo.Name, fcd.Spec.PVId)
+					log.Info(msg)
+
+					fcd.Status.SizeMB = int64(idinfo.Config.CapacityInMB)
+
+					backing := idinfo.Config.BaseConfigInfo.Backing.(*types.BaseConfigInfoDiskFileBackingInfo)
+					fcd.Status.FilePath = string(backing.FilePath)
+					fcd.Status.ProvisioningType = string(backing.ProvisioningType)
+				} else {
+					msg := fmt.Sprintf("FCDInfo: %v does not match %v", idinfo.Config.BaseConfigInfo.Name, fcd.Spec.PVId)
+					log.Info(msg)
+				}
+			}
+		}
+
+		if err := r.Status().Update(ctx, fcd); err != nil {
+			log.Error(err, "unable to update FCDInfo status")
+			return ctrl.Result{}, err
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *FCDInfoReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&topologyv1.FCDInfo{}).
+		Complete(r)
+}
---
+++ main.go
@@ -0,0 +1,225 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/find"
+	"github.com/vmware/govmomi/session/cache"
+	"github.com/vmware/govmomi/vim25"
+	"github.com/vmware/govmomi/vim25/soap"
+
+	"flag"
+	"os"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	topologyv1 "vkubeviewer/api/v1"
+	"vkubeviewer/controllers"
+	//+kubebuilder:scaffold:imports
+)
+
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	_ = clientgoscheme.AddToScheme(scheme)
+
+	_ = topologyv1.AddToScheme(scheme)
+	utilruntime.Must(topologyv1.AddToScheme(scheme))
+	// +kubebuilder:scaffold:scheme
+}
+
+//
+// - vSphere session login function
+//
+
+func vlogin(ctx context.Context, vc, user, pwd string) (*vim25.Client, *govmomi.Client, error) {
+
+	//
+	// This section allows for insecure govmomi logins
+	//
+
+	var insecure bool
+	flag.BoolVar(&insecure, "insecure", true, "ignore any vCenter TLS cert validation error")
+
+	//
+	// Create a vSphere/vCenter client
+	//
+	// The govmomi client requires a URL object, u.
+	// You cannot use a string representation of the vCenter URL.
+	// soap.ParseURL provides the correct object format.
+	//
+
+	u, err := soap.ParseURL(vc)
+
+	if u == nil {
+		setupLog.Error(err, "Unable to parse URL. Are required environment variables set?", "controller", "VMInfo")
+		os.Exit(1)
+	}
+
+	if err != nil {
+		setupLog.Error(err, "URL parsing not successful", "controller", "VMInfo")
+		os.Exit(1)
+	}
+
+	u.User = url.UserPassword(user, pwd)
+
+	//
+	// Session cache example taken from https://github.com/vmware/govmomi/blob/master/examples/examples.go
+	//
+	// Share govc's session cache
+	//
+	s := &cache.Session{
+		URL:      u,
+		Insecure: true,
+	}
+
+	//
+	// Create new client
+	//
+	c1 := new(vim25.Client)
+
+	//
+	// Login using client c and cache s
+	//
+	err = s.Login(ctx, c1, nil)
+
+	if err != nil {
+		setupLog.Error(err, "FCDInfo: vim25 login not successful", "controller", "VMInfo")
+		os.Exit(1)
+	}
+
+	c2, err := govmomi.NewClient(ctx, u, insecure)
+
+	if err != nil {
+		setupLog.Error(err, "FCDInfo: gomvomi login not successful", "controller", "VMInfo")
+		os.Exit(1)
+	}
+
+	return c1, c2, nil
+}
+
+func main() {
+	var metricsAddr string
+	var enableLeaderElection bool
+	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
+	flag.Parse()
+
+	//
+	// Retrieve vCenter URL, username and password from environment variables
+	// These are provided via the manager manifest when controller is deployed
+	//
+
+	vc := os.Getenv("GOVMOMI_URL")
+	user := os.Getenv("GOVMOMI_USERNAME")
+	pwd := os.Getenv("GOVMOMI_PASSWORD")
+
+	//
+	// Create context, and get vSphere session information
+	//
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	c1, c2, err := vlogin(ctx, vc, user, pwd)
+	if err != nil {
+		setupLog.Error(err, "unable to get login session to vSphere")
+		os.Exit(1)
+	}
+
+	//----
+
+	finder := find.NewFinder(c2.Client, true)
+
+	//
+	// -- find and set the default datacenter
+	//
+
+	dc, err := finder.DefaultDatacenter(ctx)
+
+	if err != nil {
+		setupLog.Error(err, "FCDInfo: Could not get default datacenter")
+	} else {
+		finder.SetDatacenter(dc)
+	}
+
+	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:             scheme,
+		MetricsBindAddress: metricsAddr,
+		Port:               9443,
+		LeaderElection:     enableLeaderElection,
+		LeaderElectionID:   "b610b79e.corinternal.com",
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	if err = (&controllers.FCDInfoReconciler{
+		Client: mgr.GetClient(),
+		VC1:    c1,
+		VC2:    c2,
+		Finder: finder,
+		Log:    ctrl.Log.WithName("controllers").WithName("FCDInfo"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "FCDInfo")
+		os.Exit(1)
+	}
+
+	if err = (&controllers.VMInfoReconciler{
+		Client: mgr.GetClient(),
+		VC:     c1,
+		Log:    ctrl.Log.WithName("controllers").WithName("VMInfo"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "VMInfo")
+		os.Exit(1)
+	}
+	if err = (&controllers.HostInfoReconciler{
+		Client: mgr.GetClient(),
+		VC:     c1,
+		Log:    ctrl.Log.WithName("controllers").WithName("HostInfo"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "HostInfo")
+		os.Exit(1)
+	}
+	// +kubebuilder:scaffold:builder
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
