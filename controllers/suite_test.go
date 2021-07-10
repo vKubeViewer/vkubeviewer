39ab6713bc038c1451b0fc5920e73d4b3f54d23f
JarryWang <jarrywangcn@outlook.com>
2021-07-10 14:33:17 +0100

    define spec and status for TagInfo

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -77,6 +77,9 @@ var _ = BeforeSuite(func() {
 	err = topologyv1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
+	err = topologyv1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
 	//+kubebuilder:scaffold:scheme
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
--- main.go
+++ main.go
@@ -234,6 +234,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DatastoreInfo")
 		os.Exit(1)
 	}
+	if err = (&controllers.TagInfoReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "TagInfo")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
